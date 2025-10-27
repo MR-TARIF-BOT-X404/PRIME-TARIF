8ff681a2ed1847bad3fee7904c4033a94df0f65e
MR-TARIF-BOT-X404 <mr.arafat.x404@gmail.com>
2025-10-27 13:33:08 +0600

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -1,66 +1,41 @@
 const { GoatWrapper } = require("fca-liane-utils");
-const { findUid } = global.utils;
-const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
 
 module.exports = {
-	config: {
-		name: "uid",
-		version: "1.3",
-		author: "NTKhang",
-		countDown: 5,
-		role: 0,
-		description: {
-			vi: "Xem user id facebook của người dùng",
-			en: "View facebook user id of user"
-		},
-		category: "INFORM",
-		guide: {
-			vi: "   {pn}: dùng để xem id facebook của bạn"
-				+ "\n   {pn} @tag: xem id facebook của những người được tag"
-				+ "\n   {pn} <link profile>: xem id facebook của link profile"
-				+ "\n   Phản hồi tin nhắn của người khác kèm lệnh để xem id facebook của họ",
-			en: "   {pn}: use to view your facebook user id"
-				+ "\n   {pn} @tag: view facebook user id of tagged people"
-				+ "\n   {pn} <profile link>: view facebook user id of profile link"
-				+ "\n   Reply to someone's message with the command to view their facebook user id"
-		}
-	},
+  config: {
+    name: "uid",
+    aliases: [],
+    author: "AHMED TARIF",
+    version: "1.0",
+    cooldowns: 5,
+    role: 0,
+    shortDescription: { en: "Show UID." },
+    longDescription: { en: "Displays the UID of yourself, a mention, or a replied message." },
+    category: "Inform",
+    guide: { en: "Send message, mention someone, reply, or send profile link." }
+  },
 
-	langs: {
-		vi: {
-			syntaxError: "Vui lòng tag người muốn xem uid hoặc để trống để xem uid của bản thân"
-		},
-		en: {
-			syntaxError: "Please tag the person you want to view uid or leave it blank to view your own uid"
-		}
-	},
+  onStart: async function({ api, event }) {
+    try {
+      let targetID = event.senderID;
 
-	onStart: async function ({ message, event, args, getLang }) {
-		if (event.messageReply)
-			return message.reply(event.messageReply.senderID);
-		if (!args[0])
-			return message.reply(event.senderID);
-		if (args[0].match(regExCheckURL)) {
-			let msg = '';
-			for (const link of args) {
-				try {
-					const uid = await findUid(link);
-					msg += `${link} => ${uid}\n`;
-				}
-				catch (e) {
-					msg += `${link} (ERROR) => ${e.message}\n`;
-				}
-			}
-			message.reply(msg);
-			return;
-		}
+      // Reply
+      if (event.type === "message_reply") targetID = event.messageReply.senderID;
+      // Mention
+      else if (Object.keys(event.mentions).length > 0) targetID = Object.keys(event.mentions)[0];
+      // Facebook profile link
+      else if (event.body.match(/facebook\.com\/(?:profile\.php\?id=)?(\d+)/i)) {
+        const match = event.body.match(/facebook\.com\/(?:profile\.php\?id=)?(\d+)/i);
+        targetID = match[1];
+      }
 
-		let msg = "";
-		const { mentions } = event;
-		for (const id in mentions)
-			msg += `${mentions[id].replace("@", "")}: ${id}\n`;
-		message.reply(msg || getLang("syntaxError"));
-	}
+      await api.sendMessage(`${targetID}`, event.threadID, event.messageID);
+    } catch (err) {
+      console.error(err);
+      api.sendMessage("⚠️ Could not fetch UID.", event.threadID, event.messageID);
+    }
+  }
 };
+
+// No prefix needed
 const wrapper = new GoatWrapper(module.exports);
 wrapper.applyNoPrefix({ allowPrefix: true });
